35b32e3918cd6e56e5837d38808fe4e4234b21a0
knuthelgesen <knut.helgesen@bouvet.no>
2014-07-26 17:38:24 +0200

    Move shader opening out of renderer class

--- src/main/java/no/plasmid/lib/AbstractRenderer.java
+++ src/main/java/no/plasmid/lib/AbstractRenderer.java
@@ -1,13 +1,6 @@
 package no.plasmid.lib;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.URL;
-import java.util.Scanner;
-
 import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL20;
 import org.lwjgl.util.glu.GLU;
 import org.lwjgl.util.vector.Matrix4f;
 
@@ -40,52 +33,6 @@ public abstract class AbstractRenderer {
 			final String message = "OpenGL error (" + code + "): " + errorString;
 			throw new IllegalStateException(message);
 		}
-	}
-	
-	protected void compileShader(int shaderProgramId, String source, int type) {
-		int shaderId = GL20.glCreateShader(type);
-		if (0 == shaderId) {
-			throw new IllegalStateException("Could not create shader!");
-		}
-		
-		GL20.glShaderSource(shaderId, source);
-		GL20.glCompileShader(shaderId);
-		int compileStatus = GL20.glGetShaderi(shaderId, GL20.GL_COMPILE_STATUS);
-		if (0 == compileStatus) {
-			String error = GL20.glGetShaderInfoLog(shaderId, 2048);
-			throw new IllegalStateException("Error when compiling shader: " + error);
-		}
-		
-		GL20.glAttachShader(shaderProgramId, shaderId);
-	}
-	
-	protected String loadTextFile(String fileName) throws FileNotFoundException {
-		URL fileURL = AbstractRenderer.class.getResource(fileName);
-		if (fileURL == null) {
-			throw new FileNotFoundException("Could not find shader source file " + fileName);
-		}
-		
-		StringBuilder text = new StringBuilder();
-		String NL = System.getProperty("line.separator");
-		FileInputStream fis;
-		Scanner scanner = null;
-		try {
-			fis = new FileInputStream(fileURL.getFile());
-			scanner = new Scanner(fis);
-			
-			while (scanner.hasNextLine()) {
-				text.append(scanner.nextLine() + NL);
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			if (scanner != null) {
-				scanner.close();
-			}
-		}
-		
-		return text.toString();
 	}
 	
 }
